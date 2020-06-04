f0c8ce63a44594f711110b6e6909d41a0db93dc7
Tony Thomas <thoma127@umn.edu>
2020-06-04 09:43:05 -0500

    New builder method

--- truetone/models/path_from_file.py
+++ truetone/models/path_from_file.py
@@ -1,4 +1,13 @@
+import os
+
+
 class PathFromFile:
 
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, slug):
+        self.slug = slug
+
+    @classmethod
+    def build(cls, filename_with_path):
+        filename = filename_with_path.split('/')[-1]
+        slug = os.path.splitext(filename)[0].replace("_", "-")
+        return cls(slug)
--- truetone/test/test_path_from_file.py
+++ truetone/test/test_path_from_file.py
@@ -1,7 +1,11 @@
-import pytest
 from truetone.models import path_from_file
 
 
-def test_slug():
-    path = path_from_file.PathFromFile("file-name.md")
-    assert path.path == "file-name"
+def test_slug_from_path_returns_only_slug():
+    path = path_from_file.PathFromFile.build("/path/to/file-name.md")
+    assert path.slug == "file-name"
+
+
+def test_slug_from_path_with_under_scores_returns_hyphenated_slug():
+    path = path_from_file.PathFromFile.build("/path/to/file_name.md")
+    assert path.slug == "file-name"
