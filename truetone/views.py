7364e95400c7f5ca04beb576f1ebb6e7b6fb5654
Tony Thomas <thoma127@umn.edu>
2020-06-03 19:06:23 -0500

    flake8 formatting

--- truetone/views.py
+++ truetone/views.py
@@ -3,11 +3,14 @@ from truetone import app
 from flask import render_template
 from flask import send_from_directory
 
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico',
                                mimetype='image/vnd.microsoft.icon')
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
