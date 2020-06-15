b94cea06d3ad60a188089791a754ba5d7922f2e7
Tony Thomas <thoma127@umn.edu>
2020-06-15 15:39:29 -0500

    Refactor Metadatum class
    * make the builder method do all the work

--- truetone/models/content/metadatum.py
+++ truetone/models/content/metadatum.py
@@ -9,14 +9,16 @@ class ContentMetadatum:
         with open(path, "r", encoding="utf-8") as input_file:
             fm = frontmatter.loads(input_file.read())
         slug = PathFromFile.build(path).slug
+        is_blog_entry = cls.__is_blog_entry(fm.metadata)
 
-        return cls(slug, fm.metadata)
+        return cls(slug, fm.metadata, is_blog_entry)
 
-    def __init__(self, slug, metadata):
+    @staticmethod
+    def __is_blog_entry(metadata):
+        return metadata.get('blog_entry', False) is True
+
+    def __init__(self, slug, metadata, is_blog_entry):
         self.slug = slug
         self.metadata = metadata
         self.title = metadata["title"]
-        self.is_blog_entry = self.__is_blog_entry()
-
-    def __is_blog_entry(self):
-        return self.metadata.get('blog_entry', False) is True
+        self.is_blog_entry = is_blog_entry
